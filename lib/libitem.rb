0e37f547b70102ec803851160fc58be47f14f44a
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-13 04:43:54 +0000

    automatically backed up by learn

--- lib/libitem.rb
+++ lib/libitem.rb
@@ -1,16 +1,10 @@
 class LibItem
   include Concerns::Sortable
-  
+
   attr_accessor :name
 
   def initialize(name)
     @name = name
-  end
-
-  @@all = []
-  
-  def self.all
-    @@all
   end
 
   def self.destroy_all
